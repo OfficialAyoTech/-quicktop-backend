46ec9604e8b61bdeeb4b08838f4f4f1d5d2cbaa3
Ayodeji <expensivedave49@gmail.com>
2026-07-12 04:25:54 +0100

    Use Firebase environment variables

--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,14 @@
-const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-const firebaseConfig = {
+const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-    : undefined,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
-const app =
-  getApps().length === 0
-    ? initializeApp({
-        credential: cert(firebaseConfig),
-      })
-    : getApps()[0];
+const app = initializeApp({
+  credential: cert(serviceAccount),
+});
 
 module.exports = getAuth(app);
